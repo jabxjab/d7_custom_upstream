412336ba0be99a4d667d522e6afcdd5928ca4dd9
Pantheon Managed Updates <managedupdates@pantheon.io>
2020-06-30 06:37:35 +0000

    Apply update from drops

--- index.php
+++ index.php
@@ -19,8 +19,3 @@ define('DRUPAL_ROOT', getcwd());
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 menu_execute_active_handler();
-
-
-// Update 004.
-// Update 005.
-// Update 006.
